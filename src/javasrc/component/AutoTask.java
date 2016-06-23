f0832d6631d6e8293c7bc65b784926c0af983f17
excv8426 <598867208@qq.com>
2016-06-23 20:37:21 +0800

    Websocket and sockjs demo

--- src/javasrc/component/AutoTask.java
+++ src/javasrc/component/AutoTask.java
@@ -1,9 +1,17 @@
 package javasrc.component;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javasrc.service.WSHandler;
 @Component
 public class AutoTask {
+	
+	@Autowired
+	private WSHandler wsHandler;
 	
 	/**
 	 * 定时任务。
@@ -11,6 +19,16 @@ public class AutoTask {
 	@Scheduled(cron="0 0 11 * * ?" )
 	private void runtask(){
 		
+	}
+	
+	@Scheduled(cron="*/1 * * * * ?" )
+	private void push(){
+		try {
+			wsHandler.pushDate();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
--- src/javasrc/service/WSHandler.java
+++ src/javasrc/service/WSHandler.java
@@ -1,10 +1,13 @@
 package javasrc.service;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Vector;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -43,6 +46,15 @@ public class WSHandler implements WebSocketHandler {
 	public boolean supportsPartialMessages() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	public void pushDate() throws IOException{
+		Date date=new Date();
+		String now=date.toString();
+		for (WebSocketSession session : sessions) {
+			
+			session.sendMessage(new TextMessage(now));
+		}
 	}
 	
 }
